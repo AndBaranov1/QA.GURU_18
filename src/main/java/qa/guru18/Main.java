c2a6c2c2c76fb8a4755cfb6bea9e6009b9b12dd5
Andrey_Baranov <37299544+AndBaranov1@users.noreply.github.com>
2023-02-24 16:21:01 +0700

    added getMessage method

--- src/main/java/qa/guru18/Main.java
+++ src/main/java/qa/guru18/Main.java
@@ -2,6 +2,10 @@ package qa.guru18;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.quru!");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage() {
+        return "Hello qa.quru!";
     }
 }
