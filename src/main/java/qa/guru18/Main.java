65a8da53898e78c6420942c2ef61fb06f32959b2
Andrey Baranov <37299544+AndBaranov1@users.noreply.github.com>
2023-02-24 16:42:24 +0700

    Update Main.java

--- src/main/java/qa/guru18/Main.java
+++ src/main/java/qa/guru18/Main.java
@@ -2,7 +2,7 @@ package qa.guru18;
 
 public class Main {
     
-    private static String msg = ""Hello qa.quru!!!!!"";
+    private static String msg = "Hello qa.quru!";
     
     public static void main(String[] args) {
         System.out.println(msg);
